0f135bc87db592477cb3a2e84d03fd015ecfcc2d
Maria <mineeva_maria@mail.ru>
2022-11-18 20:35:04 +0300

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит трехзначное число и удаляет среднюю цифру
+
