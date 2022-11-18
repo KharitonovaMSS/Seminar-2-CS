c4d63f7745d680c85ec4025a32ea2f1eb666e9d5
Maria <mineeva_maria@mail.ru>
2022-11-18 20:47:17 +0300

    Решена задача 11

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// Напишите программу, которая выводит трехзначное число и удаляет среднюю цифру
 
+int randomNum = new Random().Next(100,1000);
+Console.WriteLine(randomNum);
+// 238
+int num = (randomNum / 100) * 10 + randomNum % 10;
+Console.WriteLine(num);
